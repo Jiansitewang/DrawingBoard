23e2f86621901d4a335a3690324b23cfa84a11e5
Clay_wang <Clay_wang@vip.163.com>
2017-12-18 04:13:06 +0800

    添加画笔颜色、禁用触控设备滚动条

--- main.js
+++ main.js
@@ -7,16 +7,40 @@ listenToUser(yyy)
 
 
 var eraserEnabled = false
+pen.onclick = function () {
+    eraserEnabled = false
+    pen.classList.add('active')
+    eraser.classList.remove('active')
+}
 eraser.onclick = function () {
     eraserEnabled = true
-    actions.className = 'actions x'
-
-}
-brush.onclick = function () {
-    eraserEnabled = false
-    actions.className = 'actions'
+    eraser.classList.add('active')
+    pen.classList.remove('active')
 }
 
+red.onclick = function () {
+    context.fillStyle = 'red'
+    context.strokeStyle = 'red'
+    red.classList.add('active')
+    green.classList.remove('active')
+    blue.classList.remove('active')
+}
+
+green.onclick = function () {
+    context.fillStyle = 'green'
+    context.strokeStyle = 'green'
+    green.classList.add('active')
+    red.classList.remove('active')
+    blue.classList.remove('active')
+}
+
+blue.onclick = function () {
+    context.fillStyle = 'blue'
+    context.strokeStyle = 'blue'
+    blue.classList.add('active')
+    green.classList.remove('active')
+    red.classList.remove('active')
+}
 
 /******/
 
@@ -38,14 +62,12 @@ function autoSetCanvasSize(canvas) {
 
 function drawCircle(x, y, radius) {
     context.beginPath()
-    context.fillStyle = 'black'
     context.arc(x, y, radius, 0, Math.PI * 2);
     context.fill()
 }
 
 function drawLine(x1, y1, x2, y2) {
     context.beginPath();
-    context.strokeStyle = 'black'
     context.moveTo(x1, y1) // 起点
     context.lineWidth = 5
     context.lineTo(x2, y2) // 终点
@@ -67,7 +89,6 @@ function listenToUser(canvas) {
         canvas.ontouchstart = function (aaa) {
             var x = aaa.touches[0].clientX
             var y = aaa.touches[0].clientY
-            console.log(x,y)
             using = true
             if (eraserEnabled) {
                 context.clearRect(x - 5, y - 5, 10, 10)
